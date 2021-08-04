246b3278dd00a1c8e4f8ffbbca7dd609812b65e6
jojojo0501 <sarukiti0418@outlook.jp>
2021-08-04 21:47:35 +0900

    ログイン処理の実装

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -9,6 +9,7 @@ import jp.co.sample.repository.AdministratorRepository;
 
 /**
  * 管理者情報を操作するサービス
+ * 
  * @author kanekojota
  *
  */
@@ -18,13 +19,25 @@ public class AdministratorService {
 
 	@Autowired
 	private AdministratorRepository administratorRepository;
-	
+
 	/**
 	 * 管理者情報を挿入する
+	 * 
 	 * @param administrator
 	 */
 	public void insert(Administrator administrator) {
 		administratorRepository.insert(administrator);
 	}
-	
+
+	/**
+	 * ログイン処理をする
+	 * @param mailAddress
+	 * @param password
+	 * @return
+	 */
+	public Administrator login(String mailAddress, String password) {
+		Administrator administrator = administratorRepository.findByMailAddressAndPassword(mailAddress, password);
+		return administrator;
+	}
+
 }
