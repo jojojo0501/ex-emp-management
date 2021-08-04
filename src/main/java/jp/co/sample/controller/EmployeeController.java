f8584cc31cfea4ff3f5ca0cc151d929dc692631b
jojojo0501 <sarukiti0418@outlook.jp>
2021-08-04 22:52:42 +0900

    EmployeeControllerの作成

---
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package jp.co.sample.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.service.EmployeeService;
+
+/**
+ * 従業員情報を操作するコントローラー
+ * @author kanekojota
+ *
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	/**
+	 * 従業員一覧を出力する
+	 * @param model　
+	 * @return listへのフォワード
+	 */
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		model.addAttribute("employee", employeeService.showList());
+		return "employee/list";
+	}
+}
