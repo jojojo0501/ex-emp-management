9d669ecb06258388e5ea46a461b78445e8554702
jojojo0501 <sarukiti0418@outlook.jp>
2021-08-04 17:41:42 +0900

    fix Annotation of setUpInsertAdministratorForm

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -2,6 +2,7 @@ package jp.co.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.form.InsertAdministratorForm;
@@ -18,7 +19,7 @@ public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
 	
-	@RequestMapping("")
+	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
 		return new InsertAdministratorForm();
 	}
